14f406ca641354ac2a5c0614f71115b6ff68362c
kata-agata <agata.k.jazdzewska@gmail.com>
2020-10-22 12:29:04 +0200

    handlebars setup in express.js

--- src/server/express.js
+++ src/server/express.js
@@ -1,25 +1,47 @@
-import express from "express";
-import path from "path";
+const express = require('express');
+const path = require('path');
+const authRouter = require('./routes/admin/auth');
+const exphbs = require('express-handlebars');
 
 const server = express();
-const webpack = require("webpack");
-const config = require("../../config/webpack.dev.js");
-const compiler = webpack(config);
 
-const webpackDevMiddleware = require("webpack-dev-middleware")(
-  compiler,
-  config.devServer
-); // IIFE
 
-const webpackHotMiddleware = require("webpack-hot-middleware")(compiler);
+const isProd = process.env.NODE_ENV ==="production";
 
-server.use(webpackDevMiddleware);
-server.use(webpackHotMiddleware);
+if(!isProd){
+  const webpack = require("webpack");
+  const config = require("../../config/webpack.dev.js");
+  const compiler = webpack(config);
+
+  const webpackDevMiddleware = require("webpack-dev-middleware")(
+    compiler,
+    config.devServer
+  ); // IIFE
+
+  const webpackHotMiddleware = require("webpack-hot-middleware")(compiler);
+
+  server.use(webpackDevMiddleware);
+  server.use(webpackHotMiddleware);
+}
 
 const staticMiddleware = express.static("dist");
-
 server.use(staticMiddleware);
 
-server.listen(8080, ()=>{
-  console.log("Server is listening");
+server.set('views', path.join(__dirname, '/../views/'));
+server.engine('hbs',exphbs({
+  defaultLayout: 'index',
+  layoutsDir: path.join(__dirname, '/../views/layouts'),
+  partialsDir: path.join(__dirname, '/../views/partials'),
+  extname: '.hbs'
+}));
+server.set('view engine', 'hbs');
+
+
+
+
+server.use(authRouter);
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, ()=>{
+  console.log(`Server is listening on http://localhost:${PORT}`);
 });

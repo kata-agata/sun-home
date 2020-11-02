d0858b6b80fe8fb971bfe9d8f905185541502292
kata-agata <agata.k.jazdzewska@gmail.com>
2020-11-02 20:24:30 +0100

    name change of route /new to /newRealization edit added added middleware function for save and redirect

--- src/server/routes/admin/auth.js
+++ src/server/routes/admin/auth.js
@@ -3,15 +3,17 @@ const Realization = require('./../../../models/realization');
 const router = express.Router();
 
 
-
+//------------SIGN IN
 router.get('/signin', (req,res)=>{
     res.render('signin', {title: "login"});
 })
 
+//------------SHOW ADMIN PANEL MAIN PAGE
 router.get('/',(req,res)=>{
   res.render('adminPanel', {title: 'Sun-home Admin Panel'})
 })
 
+//------------LIST ALL REALIZATIONS
 router.get('/realizations', async (req,res)=>{
   let realizations = await Realization.find().sort({createdAt: 'desc'})
   realizations = realizations.map(r => r.toJSON());
@@ -21,10 +23,34 @@ router.get('/realizations', async (req,res)=>{
   })
 });
 
-router.get('/realizations/new', (req,res)=>{
-  res.render('partials/admin/addRealization', { realization: new Realization()})
+//-----------GO TO NEW REALIZATION FORM
+router.get('/realizations/newRealization', (req,res)=>{
+  res.render('partials/admin/newRealization', { realization: new Realization()})
 })
 
+//------------ SAVE NEW REALIZATION
+router.post('/realizations/newRealization', async (req,res, next)=>{
+  req.realization = new Realization() //assure that every new post has empty realization object
+  next() // going to saveAndRedirect
+}, saveAndRedirect('newRealization'))
+
+
+//-----------GO TO EDIT REALIZATION FORM
+router.get('/realizations/edit/:id', async(req,res)=>{
+  let realization = await Realization.findById(req.params.id);
+  realization = realization.toJSON();
+  res.render('partials/admin/editRealization', { realization: realization})
+})
+
+//-----------SAVE CHANGES AFTER EDIT
+router.put('/realizations/edit/:id', async (req,res, next)=>{
+  req.realization = await Realization.findById(req.params.id);
+  console.log('edit', req.realization)
+  next();
+}, saveAndRedirect('/realizations/edit'))
+
+
+//------------SHOW ONE REALIZATION
 router.get('/realizations/:slug', async (req,res)=>{
   let realization = await Realization.findOne({slug: req.params.slug});
   if ( realization === null) res.redirect('../realizations');
@@ -33,25 +59,10 @@ router.get('/realizations/:slug', async (req,res)=>{
   res.render('partials/admin/showRealization', {realization: realization});
 })
 
-router.post('/realizations/new', async (req,res)=>{
-  let realization = new Realization({
-    title: req.body.title,
-    description: req.body.description,
-    markdown: req.body.markdown
-  })
-
-  try{
-    realization = await realization.save();
-    realization = realization.toJSON();
-    console.log(realization);
-    res.redirect(`../realizations/${realization.slug}`);
-  } catch(e){
-    console.log(e);
-    res.render('partials/admin/addRealization', {realization: realization})
-  }
-})
 
 
+
+//----------- DELETE REALIZATION
 // don't delete with <a> becuse everything from database will be deleted
 //use form instead
 router.delete('/realizations/:id', async (req,res) => {
@@ -59,5 +70,25 @@ router.delete('/realizations/:id', async (req,res) => {
   await Realization.findByIdAndDelete(req.params.id);
   res.redirect('./realizations');
 })
+
+
+//------------ MIDDLEWARE FUNCTION, SAME FOR EDIT AND NEW
+function saveAndRedirect(path){
+  return async (req,res) => {
+    let realization = req.realization;
+    realization.title = req.body.title;
+    realization.description = req.body.description;
+    realization.markdown = req.body.markdown;
+    try{
+      realization = await realization.save();
+      realization = realization.toJSON();
+      console.log(realization);
+      res.redirect(`/sun/adminPanel/realizations/${realization.slug}`);
+    } catch(e){
+      console.log(e);
+      res.render(`partials/admin/${path}`, {realization: realization})
+    }
+  }
+}
 
 module.exports = router;

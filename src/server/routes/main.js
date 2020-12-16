77aca09d29161bba03518724e9b9b11fcfd73531
kata-agata <agata.k.jazdzewska@gmail.com>
2020-12-16 22:03:42 +0100

    opinon-shape as svg

--- src/server/routes/main.js
+++ src/server/routes/main.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const Realization = require('./../../models/realization');
+const Opinion = require('./../../models/opinion');
 const router = express.Router();
 
 router.get('/', async (req,res, next)=>{
   let realizations = await Realization.find().sort({createdAt: 'desc'}).catch(next);
+  let opinions = await Opinion.find().limit(4).catch(next);
 
   realizations = realizations.map(r => r.toJSON());
+  opinions = opinions.map(o => o.toJSON());
   res.render('index', {
     title: 'Sun-home: aranżacja wnętrz, remonty trójmiasto',
-    realizations: realizations
+    realizations: realizations,
+    opinions: opinions
   });
 })
 
